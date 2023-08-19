73502e2b327d23ab25f1e130d41c45884bba3da1
ilia_volodin <vollodin61@gmail.com>
2023-08-19 20:55:07 +0300

    Научил отправлять мне сообщения об ошибке с датой и временем

--- bots/autoanswer_bot/data/echo_bot_config.py
+++ bots/autoanswer_bot/data/echo_bot_config.py
@@ -1,3 +1,5 @@
+from _datetime import datetime
+
 from pyrogram import Client
 from dataclasses import dataclass
 from environs import Env
@@ -25,12 +27,11 @@ class OtherParams:
     my_id = 590018906
     tilda_chatbot_id = 265299531
     test_chat_id = -1001930780123
-
+    dt_now = datetime.now().strftime("%H:%M:%S %Y-%m-%d")
     webinar = '700 RUB'
     club = '2000 RUB'
     course = '7000 RUB'
     course_again = '3500 RUB'
-    # finded = re.findall(pattern, paragon)[0][20:]
 
     texts_for_ans_bot = {
         'if_survey': ('Здравствуйте! Я админ курса "Доброе слово для кошки" Ирины Филатовой\n'
@@ -39,7 +40,7 @@ class OtherParams:
         'if_club_again': 'Привет! Спасибо за продление участия в Клубе!)'
                          '\n\nПрисоединяйся!)\nhttps://t.me/+Yw9gMOk5i2E0YzMy ',
         'if_course': 'Здравствуйте! Я админ курса "Доброе слово для кошки" Ирины Филатовой меня зовут Илья.\n'
-                     'Спасибо за проявленный интерес и доверие! Оплата прошла успешно.\n'
+                     'Спасибо за проявленный интерес и доверие! Оплата прошла успешно.\n\n'
                      'Ссылку на чат я пришлю Вам за 2 дня до старта.\n\n'
                      'Иногда алгоритмы телеграма дают сбои. Если ссылка не пришла вовремя, то напишите мне',
         'if_course_again': 'Привет! Я всё ещё админ курса "Доброе слово для кошки", меня зовут Илья.\n'
@@ -48,7 +49,7 @@ class OtherParams:
                            'Иногда алгоритмы телеграма дают сбои. Если ссылка не пришла вовремя, то напишите мне',
     }
 
-
+print(OtherParams.dt_now)
 @dataclass
 class UserBot:
     env = Env()
--- bots/autoanswer_bot/just_go.py
+++ bots/autoanswer_bot/just_go.py
@@ -1,6 +1,5 @@
 from pyrogram import Client
 from pyrogram.types import Message
-
 from data.echo_bot_config import UserBot, OtherParams
 from utils.finder_pattern import survey_patterns_finder, order_patterns_finder
 
@@ -11,6 +10,7 @@ test_chat_id = OtherParams.test_chat_id
 text_if_survey = OtherParams.texts_for_ans_bot['if_survey']
 text_if_club = OtherParams.texts_for_ans_bot['if_club']
 text_if_course = OtherParams.texts_for_ans_bot['if_course']
+dt_now = OtherParams.dt_now
 
 
 @ubot.on_message()
@@ -34,7 +34,9 @@ def from_tilda(client: Client, message: Message):
                 ubot.send_message(chat_id=tg_nik, text=f"{text_if_survey}")
 
     except Exception as err:
-        print(f'Что-то пошло не так {err}')
+        err_text = f'\n{dt_now} - Что-то пошло не так {err}'
+        print(err_text)
+        ubot.send_message(chat_id=my_id, text=err_text)
 
 
 ubot.run()
